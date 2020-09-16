d1d0cfd294d949936b5ebd5b8a63bbc655e9f934
Pedro Capriles <caprilespe@outlook.com>
2020-09-15 23:10:42 -0300

    Independent methods CRUDs

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -9,11 +9,60 @@ const moviesApi = (app) => {
     try {
       const movies = await Promise.resolve(moviesMock);
       response.status(200).json({
-          data: movies,
-          message: 'movies was listed'
+        data: movies,
+        message: 'movies was listed',
       });
     } catch (error) {
-        next(error);
+      next(error);
+    }
+  });
+
+  router.get('/:id', async (request, response, next) => {
+    try {
+      const { id } = request.params;
+      const movie = await Promise.resolve(moviesMock.find((m) => m.id === id));
+      response.status(201).json({
+        data: movie,
+        message: 'movie retrived',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.post('/', async (request, response, next) => {
+    try {
+        const updatedMovieId = await Promise.resolve(moviesMock[0].id);
+      response.status(200).json({
+        data: updatedMovieId,
+        message: 'movie created',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.put('/:id', async (request, response, next) => {
+    try {
+      const createMovieId = await Promise.resolve(moviesMock[0].id);
+      response.status(200).json({
+        data: createMovieId,
+        message: 'movie updated',
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.delete('/:id', async (request, response, next) => {
+    try {
+      const deleteMovieId = await Promise.resolve(moviesMock[0].id);
+      response.status(200).json({
+        data: deleteMovieId,
+        message: 'movie deleted',
+      });
+    } catch (error) {
+      next(error);
     }
   });
 };
