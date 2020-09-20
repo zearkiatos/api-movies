de469b2b309cde197f0570c93cf314a9fff1bc62
Pedro Capriles <caprilespe@outlook.com>
2020-09-20 15:18:47 -0300

    Implementation actions in MongoDB

--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@ const config = {
     port: process.env.PORT || 3000,
     cors: process.env.CORS,
     dbUser: process.env.DB_USER,
+    dbPort: process.env.DB_PORT,
     dbPassword: process.env.DB_PASSWORD,
     dbHost: process.env.DB_HOST,
     dbName: process.env.DB_NAME
